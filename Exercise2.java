257e15be25bcf9dfd4277b4991f9ecc09347d95e
IGM\BekhtenovaAE <altusha.pin-up@mail.ru>
2023-08-14 23:23:49 +0700

    Exercise2

---
+++ Exercise2.java
@@ -0,0 +1,16 @@
+public class Exercise2 {
+    public static void main(String[] args) {
+
+        for (int i = 100; i >= 0; i--) {
+            multiplicity(i);
+        }
+    }
+
+    public static int multiplicity(int i) {
+        if (i % 4 == 0) {
+            System.out.print(i + " ");
+        }
+        return i;
+    }
+}
+
